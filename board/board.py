76f620fdfa2428707521ab01022f9aca1476e29f
→_→ <317286686@qq.com>
2017-12-14 20:46:13 +0800

    initialize

---
+++ board/board.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+from gomokuZero.common import *
+
+
+try:
+    range = xrange
+except NameError:
+    pass
+
+
+class Board(object):
+    def __init__(self, history=[]):
+        self.reset()
+        for action in history[:-1]:
+            self.move(action, check_flag=False)
+        for action in history[-1:]:
+            self.move(action, check_flag=True)
+
+    @property
+    def player(self):
+        return self._player
+
+    def move(self, action, check_flag=True):
+        if check_flag:
+            if action not in self.legal_actions:
+                raise Exception('illegal action:'+str(action))
+        self._board[action[0]][action[1]] = self.player
+        self.legal_actions.remove(action)
+        self._player = {BLACK: WHITE, WHITE: BLACK}[self._player]
+        self.last_action = action
+        self.history.append(action)
+        if check_flag:
+            return self.get_winner()
+        return None
+
+    def get_winner(self):
+        board = self._board
+        action = self.last_action
+        player = board[action[0]][action[1]]
+        for m_f in move_list:
+            count = 1
+            winner_pos = [action, action]
+            for sign in [-1, 1]:
+                for delta in range(1, NUMBER):
+                    r, c = m_f(action, sign*delta)
+                    if check_border((r, c)) and board[r][c] == player:
+                        count += 1
+                        winner_pos[(sign+1)/2] = (r, c)
+                    else:
+                        break
+            if count >= NUMBER:
+                self.winner = player
+                self.winner_pos = winner_pos
+                return player
+        self.winner = None if self.legal_actions else DRAW
+        self.winner_pos = []
+        return self.winner
+
+    def __getitem__(self, k):
+        if not isinstance(k, tuple) or len(k) != 2:
+            raise Exception('Unknow key when employing getitem:'+str(k))
+        return self._board[k[0]][k[1]]
+
+    def __str__(self):
+        s = ''
+        for row in self._board:
+            for v in row:
+                s += str(v) + ' '
+            s += '\n'
+        return s
+
+    __repr__ = __str__
+
+    def copy(self):
+        return self.__class__(self.history)
+
+    def reset(self):
+        self.history = []
+        self._board = [[EMPTY for _ in range(SIZE)] for _ in range(SIZE)]
+        self.legal_actions = set((r, c) for r in range(SIZE) for c in range(SIZE))
+        self._player = BLACK
+        self.last_action = None
+        self.winner = None
---
+++ common.py
@@ -0,0 +1,28 @@
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+DRAW = 3
+
+SIZE = 15
+NUMBER = 5
+
+def tolist(x):
+    if isinstance(x, list):
+        return x
+    else:
+        return [x]
+
+def check_border(action):
+    return 0 <= action[0] < SIZE and 0 <= action[1] < SIZE
+
+def move_function(action, delta):
+    return (action[0]+delta[0], action[1]+delta[1])
+
+hor_move = lambda action, d=1: move_function(action, (0, d))
+ver_move = lambda action, d=1: move_function(action, (d, 0))
+dia_move = lambda action, d=1: move_function(action, (d, d))
+bac_move = lambda action, d=1: move_function(action, (d, -d))
+
+move_list = [hor_move, ver_move, dia_move, bac_move]
+move_dict = {'hor': hor_move, 'ver': ver_move,
+             'dia': dia_move, 'bac': bac_move}
