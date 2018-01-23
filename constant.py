812288c20ce4afc68a9274a7771e491968ab3665
→_→ <317286686@qq.com>
2018-01-23 11:24:05 +0800

    update constante

--- constant.py
+++ constant.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #board
 EMPTY = 0
 BLACK = 1
@@ -6,6 +8,23 @@ DRAW = 3
 
 SIZE = 15
 NUMBER = 5
+
+BOARD_HASHING = []
+n = 2
+while len(BOARD_HASHING) < SIZE**2:
+    if len(BOARD_HASHING) == 0:
+        BOARD_HASHING.append(n)
+        continue
+    while True:
+        n += 1
+        for factor in BOARD_HASHING:
+            if n % factor == 0:
+                break
+        else:
+            BOARD_HASHING.append(n)
+            break
+del n
+del factor
 
 #preprocess
 HISTORY_STEPS = 5
