9a2f977c6760b65c7846b66bd8a619e27b68569c
→_→ <317286686@qq.com>
2018-01-24 14:08:19 +0800

    update pre-train

--- pre_train/main.py
+++ pre_train/main.py
@@ -40,10 +40,10 @@ if __name__ == '__main__':
         json_path=save_prefix + version + 'nn_config.json',
         weights_path=save_prefix + version + 'nn_weights.npz',
         optimizer_path=cache_prefix + 'optimizer.json',
-        batch_size=32,
+        batch_size=256,
         epochs=64,
         save_path=cache_prefix + 'pre_trainer.json',
         history_path='data/records/yixin_records.npz',
         blocks=2,
-        filters=64
+        filters=128
     )
