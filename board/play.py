5149bd02be0ac7baf53060e129e4741ab2024803
→_→ <317286686@qq.com>
2018-02-03 19:48:20 +0800

    update mcts

--- board/play.py
+++ board/play.py
@@ -2,11 +2,15 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from abc import ABCMeta, abstractmethod
+import sys
 import os
 import random
 import time
+import numpy as np
 from ..constant import *
 from ..utils.board_utils import *
+from ..utils.progress_bar import ProgressBar
+from ..utils import tolist
 from .board import Board
 
 
@@ -94,3 +98,54 @@ class Game(object):
             end = position2action(board.five[1])
             result += ' from {} to {}'.format(start, end)
         print(result)
+
+
+class Tournament(object):
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def play(self, game_number, batch_size, **kwargs):
+        player_1 = self.player_1
+        player_2 = self.player_2
+        progress_bar = ProgressBar(2*game_number)
+        player_1.win = 0.0
+        player_2.win = 0.0
+        step = 0
+        for black, white in [(player_1, player_2), (player_2, player_1)]:
+            current, opponent = black, white
+            boards = set()
+            count = 0
+            while count < game_number:
+                if current == black:
+                    while len(boards) < min(batch_size, game_number-count):
+                        boards.add(Board())
+                cache_boards = list(boards)
+                positions = tolist(current.get_positions(cache_boards, **kwargs))
+                finished_boards = []
+                for board, position in zip(cache_boards, positions):
+                    board.move(position)
+                    if board.is_over:
+                        if board.winner != DRAW:
+                            {BLACK: black, WHITE: white}[board.winner].win += 1.0
+                        finished_boards.append(board)
+
+                average_step = np.mean([len(board.history) for board in boards])
+                sys.stdout.write(' '*79 + '\r')
+                sys.stdout.flush()
+                sys.stdout.write(' '*60 + 'mean step:{:.0f}\r'.format(average_step))
+                sys.stdout.flush()
+
+                for board in finished_boards:
+                    boards.remove(board)
+                    count += 1
+                    step += 1
+
+                progress_bar.update(step)
+
+                current, opponent = opponent, current
+
+        sys.stdout.write(' '*79 + '\r')
+        sys.stdout.flush()
+        print('player 1 win:{:.2f}%'.format(player_1.win/2/game_number*100))
+        print('player 2 win:{:.2f}%'.format(player_2.win/2/game_number*100))
--- test/test_ai.py
+++ test/test_ai.py
@@ -1,4 +1,4 @@
-from gomokuZero.board.play import AIPlayer, Game
+from gomokuZero.board.play import AIPlayer, Game, Tournament
 from gomokuZero.model.neural_network import PolicyValueNetwork
 from gomokuZero.model.mcts import MCTS
 
@@ -9,18 +9,25 @@ from gomokuZero.model.mcts import MCTS
 nn_path = 'data/pre_train/input_coding_augmentation_version_nn_config.json'
 # nn_path = 'data/cache/cache_pre_train_version_nn_config.json'
 mcts_config = {
-    'rollout_time': 100, 'max_thread': 10, 'gamma': 0.0, 'max_depth': 4
+    'rollout_time': 100, 'max_thread': 1, 'gamma': 0.0, 'max_depth': 4
 }
-
-black_player = AIPlayer(MCTS(
+player_1 = MCTS(
     PolicyValueNetwork.load_model(nn_path), **mcts_config
-))
-# white_player = AIPlayer(MCTS(
-    # PolicyValueNetwork.load_model(nn_path), **mcts_config
-# ))
-white_player = AIPlayer(
-    PolicyValueNetwork.load_model(nn_path)
 )
+player_2 = PolicyValueNetwork.load_model(nn_path)
 
-game = Game(black_player, white_player, time_delay=2)
-game.play(Tau=0.0, verbose=2)
+tournament = Tournament(player_1, player_2)
+tournament.play(5, 5)
+
+# black_player = AIPlayer(MCTS(
+#     PolicyValueNetwork.load_model(nn_path), **mcts_config
+# ))
+# white_player = AIPlayer(MCTS(
+#     PolicyValueNetwork.load_model(nn_path), **mcts_config
+# ))
+# white_player = AIPlayer(
+#     PolicyValueNetwork.load_model(nn_path)
+# )
+#
+# game = Game(black_player, white_player, time_delay=2)
+# game.play(Tau=0.0, verbose=2)
