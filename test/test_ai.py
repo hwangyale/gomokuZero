01d63a662beefedaab07f48f319227b9329db8cd
→_→ <317286686@qq.com>
2018-02-05 22:50:14 +0800

    update pre-train

--- test/test_ai.py
+++ test/test_ai.py
@@ -12,7 +12,7 @@ mcts_config = {
     'rollout_time': 100, 'max_thread': 1, 'gamma': 0.0, 'max_depth': 4
 }
 
-case = 1
+case = 2
 if case == 1:
     player_1 = MCTS(
         PolicyValueNetwork.load_model(nn_path), **mcts_config
